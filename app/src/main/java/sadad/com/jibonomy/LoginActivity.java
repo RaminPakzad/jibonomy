7f2f9ad92240a064887ab3b58adf0daad2e70b7b
RaminPakzad <RPakzadmanesh@gmail.com>
2019-01-16 18:51:10 +0330

    add login activity and main activity

---
+++ app/src/main/java/sadad/com/jibonomy/LoginActivity.java
@@ -0,0 +1,65 @@
+package sadad.com.jibonomy;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import sadad.com.jibonomy.biz.dto.UserDto;
+import sadad.com.jibonomy.services.UserService;
+import sadad.com.jibonomy.utils.NotifyUtil;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginActivity loginActivity;
+    Button loginButton;
+    CarouselView carouselView;
+    int[] sampleImages = {R.mipmap.into_slider_04,R.mipmap.into_slider_01,R.mipmap.into_slider_02,R.mipmap.into_slider_03};
+
+    UserService userService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginActivity = this;
+        userService = new UserService();
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserDto user = userService.login();
+                SharedPreferences.Editor editor = getSharedPreferences("user", MODE_PRIVATE).edit();
+                editor.putString("username", user.getUsername());
+                editor.putString("firstName", user.getFirstName());
+                editor.putString("lastName", user.getLastName());
+                editor.putString("email", user.getEmail());
+                editor.apply();
+                Intent intent = new Intent(loginActivity, MainActivity.class);
+                loginActivity.startActivity( intent );
+            }
+        });
+
+        // Sample notification view
+        NotifyUtil.viewNotify(getApplicationContext());
+
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+}
--- app/src/main/java/sadad/com/jibonomy/MainActivity.java
+++ app/src/main/java/sadad/com/jibonomy/MainActivity.java
@@ -1,10 +1,13 @@
 package sadad.com.jibonomy;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,36 +15,99 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SeekBar.OnSeekBarChangeListener,
+        OnChartValueSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                Fragment fragment;
+                FragmentManager fragmentManager;
+                FragmentTransaction fragmentTransaction;
+                switch (item.getItemId()) {
+
+                    case R.id.wish_list:
+                       toolbar.setTitle("Wishes");
+                       loadFragment(new WishListFragment());
+
+                        break;
+                    case R.id.home:
+                        toolbar.setTitle("Home");
+                        loadFragment(new HomeFragment());
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        // Update user data in navigation bar
+        SharedPreferences prefs = getSharedPreferences("user", MODE_PRIVATE);
+        View headerView = navigationView.getHeaderView(0);
+        TextView fullName = (TextView) headerView.findViewById(R.id.user_full_name);
+        TextView email = (TextView) headerView.findViewById(R.id.user_email);
+        fullName.setText(prefs.getString("firstName","") + " " + prefs.getString("lastName",""));
+        email.setText(prefs.getString("email",""));
+
+        /*Fragment fragment = new HomeFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_body, fragment, "HomeFragment");
+        fragmentTransaction.commit();*/
+
+        Fragment fragment = new WishListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_body, fragment, "HomeFragment");
+        fragmentTransaction.commit();
+
+
     }
 
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_body, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -67,7 +133,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.add_transaction) {
             return true;
         }
 
@@ -88,14 +154,35 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
