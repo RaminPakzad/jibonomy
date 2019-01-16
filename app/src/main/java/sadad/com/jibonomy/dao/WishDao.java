e28ad2aa4920ed6b3868e831dd9aa7617a948497
RaminPakzad <RPakzadmanesh@gmail.com>
2019-01-16 17:26:39 +0330

    add wish dao and service

---
+++ app/src/main/java/sadad/com/jibonomy/dao/WishDao.java
@@ -0,0 +1,28 @@
+package sadad.com.jibonomy.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import sadad.com.jibonomy.entities.Wish;
+
+
+@Dao
+public interface WishDao {
+    @Insert
+    void insert(Wish word);
+
+    @Query("DELETE FROM Wish")
+    void deleteAll();
+
+    @Query("SELECT * from Wish ORDER BY wishId ASC")
+    List<Wish> getAllWishes();
+
+    @Query("SELECT * from Wish ORDER BY wishId ASC")
+    List<Wish> getAll();
+
+    @Query("SELECT * from Wish where  wishId = :wishId  ")
+    Wish get(Long wishId);
+}
---
+++ app/src/main/java/sadad/com/jibonomy/services/WishService.java
@@ -0,0 +1,40 @@
+package sadad.com.jibonomy.services;
+
+import android.content.Context;
+
+import java.util.List;
+
+import sadad.com.jibonomy.dao.JibonomyRoomDatabase;
+import sadad.com.jibonomy.dao.WishDao;
+import sadad.com.jibonomy.entities.Wish;
+
+/**
+ * @author ramin pakzad (RPakzadmanesh@gmail.com) on 1/16/2019.
+ */
+public class WishService {
+    private Context context;
+    private WishDao wishDao;
+
+    public WishService(Context context) {
+        JibonomyRoomDatabase db = JibonomyRoomDatabase.getDatabase(context);
+        this.wishDao = db.wishDao();
+        this.context = context;
+    }
+
+    public void insert(Wish wish) {
+        this.wishDao.insert(wish);
+    }
+
+    public List<Wish> getWishes() {
+        return wishDao.getAll();
+    }
+
+    public Wish getWish(Long wishId) {
+        return wishDao.get(wishId);
+    }
+
+    public void deleteAll() {
+        wishDao.deleteAll();
+    }
+
+}
