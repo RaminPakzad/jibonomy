f9f31620d1c2e980afeeb0d12c836044063e13ac
RaminPakzad <RPakzadmanesh@gmail.com>
2019-01-17 18:28:31 +0330

    CHANGE: in text views lable is shown always

---
+++ app/src/main/java/sadad/com/jibonomy/utils/StringUtil.java
@@ -0,0 +1,11 @@
+package sadad.com.jibonomy.utils;
+
+import java.text.DecimalFormat;
+
+/**
+ * @author ramin pakzad (RPakzadmanesh@gmail.com) on 1/17/2019.
+ */
+public class StringUtil {
+    public static DecimalFormat df = new DecimalFormat("#,###");
+
+}
